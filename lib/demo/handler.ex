beebaf71063bf664f3f42890d585559bb19d32c5
SamyakBorkar <2022bit037@sggs.ac.in>
2026-01-08 12:39:30 +0530

    Routing with the help of map pattern matching done

--- lib/demo/handler.ex
+++ lib/demo/handler.ex
@@ -18,39 +18,45 @@ defmodule Demo.Handler do
 
     %{method: method, path: path, resp_body: "", status_code: nil}
   end
+
   # route rewriting is done here if anyone is hiting with a wildlife route he will be rerouted to wildthing route
   def route_rewriting(%{path: "/wildlife"} = conv) do
     %{conv | path: "/wildthings"}
   end
+
   def route_rewriting(conv), do: conv
   # value of print ke saath saath return bhi krta h ye
   def log(conv), do: IO.inspect(conv)
 
-  def route(conv) do
-    route(conv, conv.method, conv.path)
-  end
+  # def route(conv) do
+  #   route(conv, conv.method, conv.path)
+  # end
 
   # function clauses
-  def route(conv, "GET", "/wildthings") do
+  def route(%{method: "GET", path: "/wildthings"} = conv) do
     %{conv | status_code: 200, resp_body: "Bears, Lions, Tigers"}
   end
 
   # function clauses
-  def route(conv, "GET", "/bears") do
+  def route(%{method: "GET", path: "/bears"} = conv) do
     %{conv | status_code: 200, resp_body: "EENA, MEENA, DEKKA"}
   end
 
-  def route(conv, "GET", "/bears/" <> id) do
+  def route(%{method: "GET", path: "/bears"<>id} = conv) do
     %{conv | status_code: 200, resp_body: "Bear #{id}"}
   end
 
-  def route(conv, _method, path) do
+  def route(%{path: path} = conv) do
     %{conv | status_code: 404, resp_body: "No #{path} Here !"}
   end
-  def track(%{path: path, status_code: 404}) do
-    IO.puts "Warning #{path} is on loose!"
+
+  def track(%{path: path, status_code: 404} = conv) do
+    IO.puts("Warning #{path} is on loose!")
+    conv
   end
+
   def track(conv), do: conv
+
   def format_response(conv) do
     body = conv.resp_body
     response_code = conv.status_code
