2a0c4556cb30fc942146e09d4ef51378abb4cc00
SamyakBorkar <2022bit037@sggs.ac.in>
2026-01-08 11:25:11 +0530

    handling different routes with status code rendering and with id routing : advance pattern matching done

--- lib/demo/handler.ex
+++ lib/demo/handler.ex
@@ -14,32 +14,49 @@ defmodule Demo.Handler do
       |> List.first()
       |> String.split(" ")
 
-    %{method: method, path: path, resp_body: ""}
+    %{method: method, path: path, resp_body: "", status_code: nil}
   end
 
   def log(conv), do: IO.inspect(conv) #value of print ke saath saath return bhi krta h ye
 
   def route(conv) do
-    route(conv, conv.path)
+    route(conv, conv.method, conv.path)
   end
-  def route(conv, "/wildthings") do       #function clauses
-    %{conv | resp_body: "Bears, Lions, Tigers"}
+  def route(conv, "GET", "/wildthings") do       #function clauses
+    %{conv | status_code: 200, resp_body: "Bears, Lions, Tigers"}
   end
 
-  def route(conv, "/bears") do            #function clauses
-    %{conv | resp_body: "EENA, MEENA, DEKKA"}
+  def route(conv, "GET", "/bears") do            #function clauses
+    %{conv | status_code: 200, resp_body: "EENA, MEENA, DEKKA"}
+  end
+  def route(conv, "GET", "/bears/" <> id) do
+    %{conv | status_code: 200, resp_body: "Bear #{id}"}
+  end
+  def route(conv, _method, path) do
+    %{conv | status_code: 404, resp_body: "No #{path} Here !"}
   end
 
   def format_response(conv) do
     body = conv.resp_body
+    response_code = conv.status_code
 
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{response_code} #{status_reason(response_code)}
     Content-Type: text/html
     Content-Length: #{String.length(body)}
 
     #{body}
     """
+  end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "CREATED",
+      401 => "UNAUTHORIZED",
+      404 => "NOT FOUND",
+      500 => "INTERNAL SERVER ERROR"
+    }[code]
   end
 end
 
@@ -56,6 +73,28 @@ IO.puts(response)
 
 request = """
 GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Demo.Handler.handle(request)
+IO.puts(response)
+
+request = """
+GET /unicorn HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Demo.Handler.handle(request)
+IO.puts(response)
+
+request = """
+GET /bears/1 HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
