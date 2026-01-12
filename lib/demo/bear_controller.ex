3598793b501beccf2b00f24725934acc9b1cc3c7
SamyakBorkar <2022bit037@sggs.ac.in>
2026-01-12 11:21:58 +0530

    comprehensions done

--- lib/demo/bear_controller.ex
+++ lib/demo/bear_controller.ex
@@ -1,9 +1,9 @@
 defmodule Demo.BearController do
   alias Demo.Wildthings
   alias Demo.Bear
-  defp bear_item(bear) do
-    "<li> #{bear.name} - #{bear.type}</li>"
-  end
+  @template_path Path.expand('../../templates', __DIR__)
+
+
   @spec show(%{:resp_body => any(), :status_code => any(), optional(any()) => any()}) :: %{
           :resp_body => <<_::64, _::_*8>>,
           :status_code => 200,
@@ -12,11 +12,13 @@ defmodule Demo.BearController do
   def show(conv) do
     items =
       Wildthings.list_bears()
-      |> Enum.filter(&Bear.is_grizzly/1)
       |> Enum.sort(&Bear.ord_asc_by_name/2)
-      |> Enum.map(&bear_item/1)
+
+      content = @template_path
+      |> Path.join("index.eex")
+      |> EEx.eval_file(bears: items)
 
 
-    %{conv | status_code: 200, resp_body: "<ul> #{items} </ul>"}
+    %{conv | status_code: 200, resp_body: content}
   end
 end
